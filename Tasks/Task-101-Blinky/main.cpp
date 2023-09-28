c327b297450ab30898b4a77ffcd6243464561a29
Antique-Alien <jacktprice1@gmail.com>
2023-09-28 14:18:36 +0100

    Completed Task 101 - Blinky
    After poking around for a while, I finished Blinky according to the instructions in the file.

--- Tasks/Task-101-Blinky/main.cpp
+++ Tasks/Task-101-Blinky/main.cpp
@@ -2,30 +2,42 @@
 
 // Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
 DigitalOut greenLED(LED1);
+DigitalOut blueLED(LED2);
+DigitalOut redLED (LED3);
+
+//The amount of time waited while each LED is lit
+int waitTime = 100000;
+
 
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
-//DigitalOut blueLED(LED2);
-//DigitalOut redLED(LED3);
+
 
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
 {
     // ALL the repeating code is contained in a  “while loop”
-    while(true) 
+    while(true)
     { 
         //The code between the { curly braces } is the code that is repeated forever
 
-        // Turn onboard LED ON  
-        greenLED = 1; 
+        // Turn on green & red LEDs  
+        greenLED = 1;
+        redLED = 1;
+        // Turn off blue LED
+        blueLED = 0;
 
-        // Wait 0.2 second (1 million microseconds)
-        wait_us(100000); 
+        // Wait
+        wait_us(waitTime); 
 
-        // Turn LED OFF
+        // Turn red & green LEDs off
         greenLED = 0;
+        redLED = 0;
+        // Turn blue LED on
+        blueLED = 1;
 
-        // Wait 0.2 second
-        wait_us(100000); 
+        // Wait 
+        wait_us(waitTime); 
     }
+    
 }
