acae93acb0ff575a58bfc7ccdb5b77b1925b30ce
Antique-Alien <jacktprice1@gmail.com>
2023-10-05 09:42:32 +0100

    Changed 112

--- Tasks/Task-112-Terminalnput/main.cpp
+++ Tasks/Task-112-Terminalnput/main.cpp
@@ -10,32 +10,27 @@ DigitalOut led3(LED3);
 
 int main()
 {
-    printf("\nTASK-112\n");
-
-    //getchar
-    char c1, c2;
-    printf("\n\nPress a key\n");
-    c1 = getchar();
-    printf("You entered character %c which has the ASCII code %d\n", c1, c1);
-
-    printf("\n\nPress another key\n");
-    c2 = getchar();
-    printf("You entered character %c which has the ASCII code %d\n", c2, c2);
-
-    //scanf
-    printf("\n\nTo set the delay (in ms), type in an integer number and press return\n");
-    int delay_ms;
-    int parsed = scanf("%d", &delay_ms);
+    int offDelay;
+    int onDelay;
     
-    printf("You entered %d correct integer values. The value was %dms\n", parsed, delay_ms);
+    printf("\nTASK-112\n");
+    //scanf
+    printf("\n\nTo set the off time in milliseconds, please enter an integer.\n");
+    int offParsed = scanf("%d", &offDelay);
+    printf("You entered %dms.\n",offDelay);
+    
+
+    printf("\n\nTo set the on time in milliseconds, please enter an integer.\n");
+    int onParsed = scanf("%d", &onDelay);
+    printf("You entered %dms\n",onDelay);
 
     // Stop
-    while (parsed > 0) {
-        wait_us(delay_ms * 1000);
+    while ((offParsed > 0)&&(onParsed>0)) {
+        wait_us(offDelay * 1000);
         led1 = 1;
         led2 = 1;
         led3 = 1;
-        wait_us(delay_ms * 1000);
+        wait_us(onDelay * 1000);
         led1 = 0;
         led2 = 0;
         led3 = 0;        
